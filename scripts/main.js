c18855d3535167b47452cff3fc8d7968ee15e909
Georgina Appeagyei <appeagyeigeorgina@gmail.com>
2023-02-04 16:17:51 +0000

    Clickable button on first description

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+function clickedA1() {
+    // get the button
+    var myDescription = document.getElementById('clickableA');
+    
+    // get the current value of the b
+    var displaySetting = myDescription.style.display;
+    
+    // also get the clock button, so we can change what it says
+    var myButton = document.getElementById('clickable1')
+
+    // now toggle the description and button text, depending on the current state
+    if (displaySetting == 'block') {
+        //description is visible - hide it
+        myDescription.style.display = 'none';
+        // change button text
+        myButton.innerHTML = 'Read description';
+    } else {
+        // description is hidden - show it
+        myDescription.style.display = 'block';
+        //change button text
+        myButton.innerHTML = 'Hide description';
+    }
+}
