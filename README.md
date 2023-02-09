c9c26d3973eb9a3c93e2ec10d36bacad609d6061
Georgina Appeagyei <appeagyeigeorgina@gmail.com>
2023-02-09 18:37:43 +0000

    Remaining buttons functioning and CSS style change

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project-gallery
+This site hosts the projects I have worked on for my application submission to Fonders and Coders. First I created 5 section tags and nested image and h2 elements. Then I uploaded placeholder images. After I used CSS flexbox to line the sections along the 'main' axis. I also used if statements in JavaScript to toggle the description. Finally, I styled the page changing the font, background colour and removed the top margin of the h2 element.
+
+Edit: after putting in some text, I realised the text overflowed so I set the maximum width of the paragraph element equal to the images.
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,11 @@
-
-
+// buttonA toggle function
 function clickedA() {
-    // get the button
+    // get the description
     var myDescription = document.getElementById('descriptionA');
-    
-    // get the current value of the b
+    // get the button, so we can change what it says
+    var myButton = document.getElementById('buttonA');
+    // get the current display value of the button
     var displaySetting = myDescription.style.display;
-    
-    // also get the clock button, so we can change what it says
-    var myButton = document.getElementById('buttonA')
-
     // now toggle the description and button text, depending on the current state
     if (displaySetting == 'block') {
         //description is visible - hide it
@@ -23,3 +19,102 @@ function clickedA() {
         myButton.innerHTML = 'Hide description';
     }
 }
+
+// buttonB toggle function
+function clickedB() {
+    // get the description
+    var myDescription = document.getElementById('descriptionB');
+    // get the button, so we can change what it says
+    var myButton = document.getElementById('buttonB');
+    // get the current display value of the button
+    var displaySetting = myDescription.style.display;
+    // now toggle the description and button text, depending on the current state
+    if (displaySetting == 'block') {
+        //description is visible - hide it
+        myDescription.style.display = 'none';
+        // change button text
+        myButton.innerHTML = 'Read description';
+    } else {
+        // description is hidden - show it
+        myDescription.style.display = 'block';
+        //change button text
+        myButton.innerHTML = 'Hide description';
+    }
+}
+
+// buttonC toggle function
+function clickedC() {
+    // get the description
+    var myDescription = document.getElementById('descriptionC');
+    // get the button, so we can change what it says
+    var myButton = document.getElementById('buttonC');
+    // get the current display value of the button
+    var displaySetting = myDescription.style.display;
+    // now toggle the description and button text, depending on the current state
+    if (displaySetting == 'block') {
+        //description is visible - hide it
+        myDescription.style.display = 'none';
+        // change button text
+        myButton.innerHTML = 'Read description';
+    } else {
+        // description is hidden - show it
+        myDescription.style.display = 'block';
+        //change button text
+        myButton.innerHTML = 'Hide description';
+    }
+}
+
+// buttonD toggle function
+function clickedD() {
+    // get the description
+    var myDescription = document.getElementById('descriptionD');
+    // get the button, so we can change what it says
+    var myButton = document.getElementById('buttonD');
+    // get the current display value of the button
+    var displaySetting = myDescription.style.display;
+    // now toggle the description and button text, depending on the current state
+    if (displaySetting == 'block') {
+        //description is visible - hide it
+        myDescription.style.display = 'none';
+        // change button text
+        myButton.innerHTML = 'Read description';
+    } else {
+        // description is hidden - show it
+        myDescription.style.display = 'block';
+        //change button text
+        myButton.innerHTML = 'Hide description';
+    }
+}
+
+// buttonE toggle function
+function clickedE() {
+    // get the description
+    var myDescription = document.getElementById('descriptionE');
+    // get the button, so we can change what it says
+    var myButton = document.getElementById('buttonE');
+    // get the current display value of the button
+    var displaySetting = myDescription.style.display;
+    // now toggle the description and button text, depending on the current state
+    if (displaySetting == 'block') {
+        //description is visible - hide it
+        myDescription.style.display = 'none';
+        // change button text
+        myButton.innerHTML = 'Read description';
+    } else {
+        // description is hidden - show it
+        myDescription.style.display = 'block';
+        //change button text
+        myButton.innerHTML = 'Hide description';
+    }
+}
+
+//execute event listener "click" on button A
+var readDescription = document.getElementById('buttonA').addEventListener("click", clickedA);
+//execute event listener "click" on button B
+var readDescription = document.getElementById('buttonB').addEventListener("click", clickedB);
+//execute event listener "click" on button C
+var readDescription = document.getElementById('buttonC').addEventListener("click", clickedC);
+//execute event listener "click" on button D
+var readDescription = document.getElementById('buttonD').addEventListener("click", clickedD);
+//execute event listener "click" on button E
+var readDescription = document.getElementById('buttonE').addEventListener("click", clickedE);
